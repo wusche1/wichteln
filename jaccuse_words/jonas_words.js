eecde44ba287794fec4c37b79163b8c100dd9512
JULIANS_LAPTOP\wusch <julianartur.schulz@stud.uni-goettingen.de>
2024-09-07 16:15:05 +0200

    fixed jonas words

--- jaccuse_words/jonas_words.js
+++ jaccuse_words/jonas_words.js
@@ -1,16 +1,16 @@
 place_list.push(
     ["Security check (airport)","Border control (airport)","jonas_words"],
     ["Corner shop","Vending machine","jonas_words"],
-    ["Gate (airport)","Platform (train station)”,”jonas_words"],
-    ["Backpackers hostel","Mountain hut (for hikers)”,”jonas_words"],
+    ["Gate (airport)","Platform (train station)","jonas_words"],
+    ["Backpackers hostel","Mountain hut (for hikers)","jonas_words"],
     ["Dormitory","Youth hostel","jonas_words"],
     ["Ballroom","Drawing room","jonas_words"],
     ["Outhouse (de: Plumsklo)","Porta Potty (at construction sites, de: Dixi-Klo)","jonas_words"],
-    ["LinkedIn”,”Twitter”,”jonas_words”],
-    ["Fiancee”,”War bride”,”jonas_words”],
-    ["9/11 memorial”,”One World Trade Center”,”jonas_words”],
-    ["Menstrual cramps”,”Labour pains”,”jonas_words”],
-    ["Music festival”,”Rave (dance party)”,”jonas_words”],
-    ["Hitchhiking”,”Hijacking”,”jonas_words”],
-    ["Remix”,”Medley”,”jonas_words"]
+    ["LinkedIn","Twitter","jonas_words"],
+    ["Fiancee","War bride","jonas_words"],
+    ["9/11 memorial","One World Trade Center","jonas_words"],
+    ["Menstrual cramps","Labour pains","jonas_words"],
+    ["Music festival","Rave (dance party)","jonas_words"],
+    ["Hitchhiking","Hijacking","jonas_words"],
+    ["Remix","Medley","jonas_words"]
 );
