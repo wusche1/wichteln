82025476ff8e0ca910c0ee1c9905b9b089327615
JULIANS_LAPTOP\wusch <julianartur.schulz@stud.uni-goettingen.de>
2024-09-07 16:09:48 +0200

    evas words

---
+++ jaccuse_words/eva_words.js
@@ -0,0 +1,13 @@
+place_list.push(
+    ["Brettspielmesse","Gamescom","eva_words"],
+    ["Bauernhof","Streichelzoo","eva_words"],
+    ["Second Hand shop","Begehbarer Kleiderschrank","eva_words"],
+    ["Baumarkt","Hochseilgarten","eva_words"],
+    ["Flussbiegung","Deich","eva_words"],
+    ["Meeresboden","Raumschiff","eva_words"],
+    ["Dungeon","Labyrinth","eva_words"],
+    ["Streuobstwiese","Maisfeld","eva_words"],
+    ["Safari","Jungel","eva_words"],
+    ["Hochzeit","Jahrmarkt","eva_words"],
+    ["Gefängnis","U-Boot","eva_words"],
+);
