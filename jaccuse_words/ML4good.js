0a3328047e417fa3b5aca65d3690dbd1ebbfe47f
JULIANS_LAPTOP\wusch <julianartur.schulz@stud.uni-goettingen.de>
2024-09-26 13:16:50 +0200

    ML4good words

---
+++ jaccuse_words/ML4good.js
@@ -0,0 +1,17 @@
+place_list.push(
+    ["Terminator","Transformer","AI_safety_beginner"],
+    ["University","ML4good","AI_safety_beginner"],
+    ["Machine Learning","pytorch","AI_safety_beginner"],
+    ["X-Risk", "The Singularity", "AI_safety_beginner"],
+    ["Alan Turing","Sam Altman","AI_safety_beginner"],
+    ["Nvidia","(Google) DeepMind","AI_safety_beginner"],
+    ["Paperclip Maximizer", "Instrumental Convergence", "AI_safety_advanced"],
+    ["Reward Hacking", "Goodhart's Law", "AI_safety_advanced"],
+    ["Nick Bostrom", "Stuart Russell", "AI_safety_advanced"],
+    ["Longtermism", "Effective Accelerationism (e/acc)", "AI_safety_advanced"],
+    ["Attention Head", "Feature", "AI_safety_advanced"],
+    ["Reward Function", "Gradient Descent", "AI_safety_advanced"],
+    ["Paperclips", "Coherent Extrapolated Volition (CEV)", "AI_safety_advanced"],
+    ["GPU", "GPT", "AI_safety_advanced"],
+    ["Corrigibility", "Interpretability", "AI_safety_advanced"],
+);
