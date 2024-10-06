899b257170f06ee82997340f1e0adda84a231883
JULIANS_LAPTOP\wusch <julianartur.schulz@stud.uni-goettingen.de>
2024-10-06 15:41:34 +0200

    started implementing loggin with google

--- java_scripts/google_login.js
+++ java_scripts/google_login.js
@@ -1,25 +1,5 @@
-function initializeGoogleSignIn() {
-  console.log("Initializing Google Sign-In");
-  console.log("Current origin:", window.location.origin);
-  console.log("Client ID:", "663759759562-koons6upuoj2n06l9ncf7of7c00b4qhf.apps.googleusercontent.com");
-
-  google.accounts.id.initialize({
-    client_id: "663759759562-koons6upuoj2n06l9ncf7of7c00b4qhf.apps.googleusercontent.com",
-    callback: handleCredentialResponse
-  });
-
-  console.log("Google Sign-In initialized");
-
-  google.accounts.id.renderButton(
-    document.getElementById("g_id_signin"),
-    { theme: "outline", size: "large" }
-  );
-
-  console.log("Sign-In button rendered");
-
-  google.accounts.id.prompt();
-
-  console.log("One Tap prompt displayed");
-}
-
-document.addEventListener("DOMContentLoaded", initializeGoogleSignIn);
+function handleCredentialResponse(response) {
+      user_name = response.getBasicProfile().getName();
+      user_email = response.getBasicProfile().getEmail();
+      console.log("User name: " + user_name + " User Email: " + user_email);
+    }
