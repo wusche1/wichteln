9b15380e5a9e5dec9a9d66207feadc3fe2a689d4
JULIANS_LAPTOP\wusch <julianartur.schulz@stud.uni-goettingen.de>
2024-10-06 16:11:37 +0200

    started implementing loggin with google

--- java_scripts/google_login.js
+++ java_scripts/google_login.js
@@ -1,8 +1,11 @@
 // Define the callback function in the global scope
-window.handleCredentialResponse = function(response) {
+function handleCredentialResponse(response) {
   console.log("Encoded JWT ID token: " + response.credential);
   // Here you can process the token
-};
+}
+
+// Ensure the function is attached to the window object
+window.handleCredentialResponse = handleCredentialResponse;
 
 // Wait until the DOM is fully loaded
 document.addEventListener('DOMContentLoaded', function() {
